070d6cd149f0b32ed5648505e620e5622899d68f
Paulo <43105@alunos.upt.pt>
2023-03-29 10:24:42 +0100

    version 1.0

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+
 
 eq = pd.read_csv("earthquakes.csv")
+
+print("\n INFO:")
+print(eq.info())
+print("\n SHAPE:")
+print(eq.shape)
+print("\n COLUNAS:")
+print(eq.columns)
+print("\n TAMANHO:")
+print(eq.size)
+print("\n DESCRIBE:")
+print(eq.describe())
+print("\n NULOS:")
+print(eq.isnull().sum())
+
+
+
+
 
